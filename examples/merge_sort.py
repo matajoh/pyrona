c4a99a00aabe6b34fbe63b38573ec5a9bf127088
Matthew A Johnson <matjoh@microsoft.com>
2023-07-17 17:53:22 +0100

    More BoC (#7)
    Signed-off-by: Matthew A Johnson <matjoh@microsoft.com>

--- examples/merge_sort.py
+++ examples/merge_sort.py
@@ -1,7 +1,6 @@
 """Example showing merge sort using BoC."""
 
 import random
-from time import sleep
 
 from pyrona import Region, wait, when
 
@@ -14,80 +13,62 @@ def _sort_section(source: tuple, start: int, end: int, output: Region):
         return
 
     if end - start + 1 <= Threshold:
-        print("sorting", start, end)
-        # below a threshold, we use the built-in sort function
-        values = list(sorted(source[start:end + 1]))
-
         # when output:
         @when(output)
         def _():
-            print("copying", start, end)
-            # we add a random sleep to make the concurrency a bit
-            # more interesting
-            sleep(random.random() / 10)
-            output.values[start:end + 1] = values
+            print("sorting", start, end)
+            output.values = list(sorted(source[start:end + 1]))
 
         return
 
+    lhs = Region("lhs").make_shareable()
+    rhs = Region("rhs").make_shareable()
     mid = (start + end) // 2
-    _sort_section(source, start, mid, output)
-    _sort_section(source, mid + 1, end, output)
+    _sort_section(source, start, mid, lhs)
+    _sort_section(source, mid + 1, end, rhs)
 
     # when output:
-    @when(output)
+    @when(output, lhs, rhs)
     def _():
         print("merging", start, end)
-        # we add a random sleep to make the concurrency a bit
-        # more interesting
-        sleep(random.random() / 10)
 
-        # merge in place
-        i = start
-        j = mid + 1
-        k = mid
-
-        if output.values[k] <= output.values[j]:
-            return
-
-        while i <= k and j <= end:
-            if output.values[i] <= output.values[j]:
+        i = 0
+        j = 0
+        values = []
+        while i < len(lhs.values) and j < len(rhs.values):
+            if lhs.values[i] < rhs.values[j]:
+                values.append(lhs.values[i])
                 i += 1
             else:
-                value = output.values[j]
-                index = j
-
-                while index != i:
-                    output.values[index] = output.values[index - 1]
-                    index -= 1
-
-                output.values[i] = value
-
-                i += 1
+                values.append(rhs.values[j])
                 j += 1
-                k += 1
+
+        while i < len(lhs.values):
+            values.append(lhs.values[i])
+            i += 1
+
+        while j < len(rhs.values):
+            values.append(rhs.values[j])
+            j += 1
+
+        output.values = values
 
 
 def _main():
-    # Create a region to hold the output
-    r = Region("MergeSort")
-
     # Create an immutable list of integers as input
     values = tuple([random.randint(0, 100) for _ in range(100)])
     print("unsorted:", values)
 
-    # Synchronously set up the region
-    with r:
-        r.values = [0] * len(values)
-
-    r.make_shareable()
+    # Create a region to hold the output
+    output = Region("MergeSort").make_shareable()
 
     # Sort the list
-    _sort_section(values, 0, len(values) - 1, r)
+    _sort_section(values, 0, len(values) - 1, output)
 
     # when r:
-    @when(r)
+    @when(output)
     def _():
-        print("sorted:", r.values)
+        print("sorted:", output.values)
 
 
 if __name__ == "__main__":
