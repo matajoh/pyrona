c4ba9a681cc368df81df959d3ca69bd184c07466
Matthew A Johnson <matjoh@microsoft.com>
2024-10-11 22:57:42 +0100

    Adding some more tests
    Signed-off-by: Matthew A Johnson <matjoh@microsoft.com>

--- tests/test_notices.py
+++ tests/test_notices.py
@@ -37,6 +37,31 @@ def test_counting():
     wait()
 
 
+def test_ordering():
+    notice_register(["foo", "bar"])
+    notice_exchange("foo", 42)
+    notice_exchange("bar", 24)
+
+    for _ in range(10):
+        @when()
+        def _():
+            assert notice_read("foo") == 42
+            assert notice_read("bar") == 24
+
+    @when()
+    def _():
+        notice_exchange("foo", 24)
+        notice_exchange("bar", 42)
+
+    for _ in range(10):
+        @when()
+        def _():
+            assert notice_read("foo") == 24
+            assert notice_read("bar") == 42
+
+    wait()
+
+
 def test_not_immutable():
     notice_register(["foo"])
 
@@ -46,3 +71,28 @@ def test_not_immutable():
         pass
     else:
         raise AssertionError("Expected ValueError.")
+
+
+def test_exists():
+    notice_register(["foo"])
+
+    try:
+        notice_read("bar")
+    except KeyError:
+        pass
+    else:
+        raise AssertionError("Expected KeyError.")
+
+    try:
+        notice_exchange("bar", 42)
+    except KeyError:
+        pass
+    else:
+        raise AssertionError("Expected KeyError.")
+
+    try:
+        notice_compare_exchange("bar", 42, 0)
+    except KeyError:
+        pass
+    else:
+        raise AssertionError("Expected KeyError.")
